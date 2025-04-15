a497e8ed88e2aed69ed3d36afe8c4bbef66ac712
Jay Davis <jdlikesbsbl@gmail.com>
2025-04-15 09:59:38 -0500

    adds working models and signup, checksession

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,10 +8,54 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        if not username or not password:
+            return {"error": "Username and password are required"}, 422
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {"error": "username already exists"}, 409
+        
+        try:
+            new_user = User(
+                username = username,
+                image_url = image_url,
+                bio = bio
+            )
+            new_user.password_hash = password
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return new_user.to_dict(), 201
+
+        except:
+            return {"error": "something went wrong"}, 500
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = db.session.get(User, user_id)
+
+            if user:
+                return {
+                    "id": user.id,
+                    "username": user.username,
+                    "image_url": user.image_url,
+                    "bio": user.bio
+                }, 200
+        
+        else:
+            return {"error": "Unauthorized"}, 401
 
 class Login(Resource):
     pass
